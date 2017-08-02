adca9c70a518e3786e9d9ce104c8ccce469e5472
Matthew Cohen <matt@mattbsg.xyz>
2017-08-02 19:00:46 -0400

    English local file

---
+++ localization/english.py
@@ -0,0 +1,32 @@
+#############################################################
+#         This is the english localization file             #
+#       use this as a template for other languages          #
+#############################################################
+
+
+# Begin bot.py
+
+
+
+
+# Begin utils.py
+
+
+
+
+# Begin register.py
+
+
+
+
+# Begin constants.py
+
+
+
+
+# Begin config.py
+
+
+
+
+# 
