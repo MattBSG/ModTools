b802f4d12af779951a56c04a70197cfeb4bfa70c
Ian Murray <theianm2002@ymail.com>
2017-08-08 15:36:20 -0400

    Rewrite language handler to use configparser.

--- locale/__init__.py
+++ locale/__init__.py
@@ -1,17 +1,25 @@
-#from automod.bot import AutoMod
-class Lang():
-    available_langs = ["en", "es"]
-    tdays = 'days'
-    tday = 'day'
-    thours = 'hours'
-    thour = 'hour'
-    tminutes = 'minutes'
-    tminute = 'minute'
-    tseconds = 'seconds'
-    tsecond = 'second'
-    end_of_init = 'end of init'
-    backup_config_except = 'Server ID: {}\n\nConfig:\n{}'
+import importlib, configparser, glob, os
 
-    def lang_control():
-    #DO STUFF
-        return
+lang_config = configparser.ConfigParser()
+lang_config.read('config/options.txt')
+lang_setting = lang_config.get('Language', 'Language', fallback='en')
+
+if not os.path.isfile("locale/{}.txt".format(lang_setting)):
+    print("Invaild language: {}".format(lang_setting))
+    lang_setting = "en"
+
+lang_strs = {}
+
+for file in glob.glob("locale/*.txt"):
+    language_file = configparser.ConfigParser()
+    language_file.read('{}'.format(file))
+    lang_strs[file[7:-4]] = dict(language_file.items('language'))
+
+class Lang():
+    def bot_lang_control(string):
+        try:
+            lang_string = lang_strs[lang_setting][string]
+        except:
+            print("Missing translation '{}' in language '{}'".format(string, lang_setting))
+            lang_string = string
+        return lang_string
